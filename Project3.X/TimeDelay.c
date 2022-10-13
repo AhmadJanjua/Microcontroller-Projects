310f1b71f34a3996e89f225a28100736c68b3f35
Kazmi20 <98751204+Kazmi20@users.noreply.github.com>
2022-10-13 14:16:10 -0600

    Update TimeDelay.c

--- Project3.X/TimeDelay.c
+++ Project3.X/TimeDelay.c
@@ -8,22 +8,39 @@
 
 #include "TimeDelay.h"
 
-void delay_ms(int time_ms) 
-{
-    //T2CON Configuration
-    T2CON<3> = 0;
-    T2CON<15> = 0;
-    
-    //Timer 2 interrupt config
-    // Compute PR2
-    Idle(); //Stays here till interrupt
-    return;
-}
+void delay_ms(int delay);
 
+// Timer 2 interrupt subroutine
 void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void)
 {
-    IFS0bits.T2IF=0; //Clear timer 2 interrupt flag
-    T2CONbits.TON=0; //stop timer
-    TMR2flag = 1; // global variable created by user
-    return;
+IFS0bits.T2IF=0; //Clear timer 2 interrupt flag
+T2CONbits.TON=0; //stop timer
+// TMR2flag = 1; // global variable created by user
 }
+
+
+void delay_ms(int delay){
+    T2CONbits.TON=1; // Starting the clock.
+    
+    IPC1bits.T2IP0=0;
+    IPC1bits.T2IP1=1;
+    IPC1bits.T2IP2=0; // setting the priority level as 2
+    
+    IEC0bits.T2IE=1; //enable time interrupt
+    IFS0bits.T2IF=0; //flag is disabled.
+    
+    PR2=16000 * delay;
+    
+    Idle();
+    return;
+    
+    
+    
+    
+    
+}
+
+
+
+
+
